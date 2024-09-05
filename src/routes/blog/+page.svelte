e43bacbd42aa8a83c6c7bc7663c82404b2652bd1
siiiiisar <shunsuke.ue@gmail.com>
2024-09-05 21:55:20 +0900

    BlogCard追加

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,2 +1,33 @@
+<script context="module" lang="ts">
+  export interface Blog {
+    title: string,
+    createdAt: string,
+    eyeCatchUrl: string
+  }
+</script>
+
+<script lang="ts">
+	import BlogCard from "../../components/BlogCard.svelte";
+
+  // TODO APIで取得
+  const blogList: Blog[] = [
+    {
+      title: "ブログタイトル",
+      createdAt: "2024-08-14T14:54:08.384Z",
+      eyeCatchUrl: "https://images.microcms-assets.io/assets/6b73b0467f254d3a9f5bf55dc020a88e/2af9e9fc33d24c07a9badbac57fd28fd/siiiiisar.webp"
+    },
+    {
+      title: "ブログタイトル2",
+      createdAt: "2024-08-15T14:54:08.384Z",
+      eyeCatchUrl: "https://images.microcms-assets.io/assets/6b73b0467f254d3a9f5bf55dc020a88e/2af9e9fc33d24c07a9badbac57fd28fd/siiiiisar.webp"
+    },
+  ];
+</script>
+
 <h1>Blogs</h1>
 <p>ブログ</p>
+{#each blogList as blog}
+  <BlogCard 
+    {blog}
+  />
+{/each}
