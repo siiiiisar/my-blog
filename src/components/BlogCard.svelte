ad16ca69fe96f22587ccc53fbfa7cac43941f282
siiiiisar <shunsuke.ue@gmail.com>
2024-09-08 22:50:48 +0900

    ブログ表示部分のデザイン調整

--- src/components/BlogCard.svelte
+++ src/components/BlogCard.svelte
@@ -4,43 +4,23 @@
 	export let content: Blog;
 </script>
 
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow overflow-hidden">
-	<div class="aspect-h-2 aspect-w-5 xs:aspect-h-2 xs:aspect-w-7 sm:aspect-none">
+<div class="bg-white border border-gray-200 rounded-lg shadow overflow-hidden">
+	<div class="aspect-h-2 aspect-w-5">
 		<a href="blogs/{content.id}">
 			<img
 				class="rounded-t-lg h-full w-full object-cover object-center"
 				src={content.eyecatch?.url}
-				alt=""
+				alt={content.title}
+				loading="lazy"
 			/>
 		</a>
 	</div>
-	<div class="p-5">
+	<div class="p-5 bg-gradient-to-br from-violet-900 to-violet-900/50">
 		<a href="blogs/{content.id}">
-			<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+			<h5 class="mb-2 font-bold tracking-tight text-base">
 				{content.title}
 			</h5>
 		</a>
-		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{content.createdAt}</p>
-		<a
-			href="blogs/{content.id}"
-			class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-		>
-			Read more
-			<svg
-				class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-				aria-hidden="true"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 14 10"
-			>
-				<path
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M1 5h12m0 0L9 1m4 4L9 9"
-				/>
-			</svg>
-		</a>
+		<p class="mb-3 font-normal text-base">{content.createdAt}</p>
 	</div>
 </div>
--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -7,6 +7,10 @@
 
 <h1>Blogs</h1>
 <p>ブログ</p>
-{#each data.contents as content}
-	<BlogCard {content} />
-{/each}
+<div class="mx-auto max-w-screen-lg px-3 py-6">
+	<div class="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3">
+		{#each data.contents as content}
+			<BlogCard {content} />
+		{/each}
+	</div>
+</div>
