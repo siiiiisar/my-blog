3df3fde1cc7e5ad16fa91d95c86dadc04e9d0806
siiiiisar <shunsuke.ue@gmail.com>
2024-09-21 22:06:33 +0900

    ブログカードのデザイン修正

--- src/components/BlogCard.svelte
+++ src/components/BlogCard.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
 	import type { Blog } from '$lib/microcms';
+	import { page } from '$app/stores';
 
 	export let content: Blog;
 </script>
 
 <div class="bg-white border border-gray-200 rounded-lg shadow overflow-hidden">
-	<div class="aspect-h-2 aspect-w-5">
+	<div class="aspect-h-9 aspect-w-16">
 		<a href="blogs/{content.id}">
 			<img
 				class="rounded-t-lg h-full w-full object-cover object-center"
-				src={content.eyecatch?.url}
+				src={content.eyecatch?.url ?? $page.url.origin + "/blog_no_image.jpg"}
 				alt={content.title}
 				loading="lazy"
 			/>
 		</a>
 	</div>
-	<div class="p-5 bg-gradient-to-br from-violet-900 to-violet-900/50">
+	<div class="p-5 bg-violet-300">
 		<a href="blogs/{content.id}">
-			<h5 class="mb-2 font-bold tracking-tight text-base">
+			<p class="mb-2 font-bold tracking-tight text-lg">
 				{content.title}
-			</h5>
+			</p>
 		</a>
-		<p class="mb-3 font-normal text-base">{content.createdAt}</p>
+		<p class="mb-3 font-normal text-sm">{content.createdAt}</p>
 	</div>
 </div>
