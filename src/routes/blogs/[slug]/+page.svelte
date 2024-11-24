944549063ea780d9ef239b04e2177b47e7203734
siiiiisar <shunsuke.ue@gmail.com>
2024-11-24 18:02:11 +0900

    iframeが初回画面遷移時に表示されないバグ修正

--- src/routes/blogs/[slug]/+page.svelte
+++ src/routes/blogs/[slug]/+page.svelte
@@ -9,6 +9,10 @@
 {#if data.meta !== undefined}
 	<Head pageTitle={data.title} description={data.meta.description} />
 {/if}
+<svelte:head>
+  <script async src="//cdn.iframe.ly/embed.js" charset="utf-8"></script>
+  <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</svelte:head>
 
 <CommonLayout title={data.title}>
 	<article class="znc p-16 rounded-lg bg-white">
