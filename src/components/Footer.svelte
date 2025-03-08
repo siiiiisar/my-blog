2051998bffedbceac5bb850c3efda7ea975c2802
siiiiisar <shunsuke.ue@gmail.com>
2025-03-08 21:57:35 +0900

    Copyright更新

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,13 +3,7 @@
 		<p
 			class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
 		>
-			&copy; Copyright 2024 by
-			<a
-				href="https://x.com/siiiiisar"
-				class="text-gray-600 ml-1"
-				rel="noopener noreferrer"
-				target="_blank">siiiisar</a
-			>
+			&copy; 2024 - 2025 siiiiisar. All rights reserved.
 		</p>
 	</div>
 </footer>
