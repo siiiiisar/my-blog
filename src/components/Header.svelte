bace34b2a13b85d4bc1bfe02faf7ef0338cbb2eb
siiiiisar <shunsuke.ue@gmail.com>
2026-04-30 00:53:46 +0900

    ヘッダデザインのリファクタ

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,13 +2,51 @@
 	import GradientText from './GradientText.svelte';
 </script>
 
-<header class="sticky top-0 z-10 bg-white/80 backdrop-blur-sm">
-	<div class="container mx-auto flex items-center justify-between p-5 text-lg">
-		<a class="flex items-center font-medium" href="/">
-			<span class="text-3xl font-bold"><GradientText text="siiiiisar"></GradientText></span>
+<header class="z-40">
+	<div class="absolute m-5 h-12 w-12 select-none outline-none xl:fixed">
+		<a href="/">
+			<span class="text-3xl font-bold">
+				<GradientText text="siiiiisar" />
+			</span>
 		</a>
-		<nav class="flex items-center space-x-6">
-			<a href="/about" class="text-gray-600 transition-colors hover:text-gray-900"> About </a>
-		</nav>
 	</div>
+	<nav class="nav">
+		<div class="spacer"></div>
+		<div class="right">
+			<a href="/about"> About </a>
+		</div>
+	</nav>
 </header>
+
+<style>
+	.nav {
+		padding: 2rem;
+		width: 100%;
+		display: grid;
+		grid-template-columns: auto max-content;
+		box-sizing: border-box;
+	}
+	.nav > * {
+		margin: auto;
+	}
+	.nav a {
+		text-decoration: none;
+		color: inherit;
+		transition: opacity 0.2s ease;
+		opacity: 0.6;
+		user-select: none;
+		outline: none;
+	}
+	.nav a:hover {
+		opacity: 1;
+		text-decoration-color: inherit;
+	}
+	.nav .right {
+		display: grid;
+		grid-gap: 1.2rem;
+		grid-auto-flow: column;
+	}
+	.nav .right > * {
+		margin: auto;
+	}
+</style>
