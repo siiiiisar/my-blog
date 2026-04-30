c7b020d5b9b074630e03870e6f8263ee81e2181f
siiiiisar <shunsuke.ue@gmail.com>
2026-04-30 17:39:27 +0900

    背景追加

--- src/components/CommonLayout.svelte
+++ src/components/CommonLayout.svelte
@@ -1,3 +1,20 @@
+<div class="grid-bg"></div>
 <div class="content m-auto mb-8">
 	<slot />
 </div>
+
+
+<style>
+.grid-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background-image: 
+    linear-gradient(to right, #8882 1px, transparent 1px),
+    linear-gradient(to bottom, #8882 1px, transparent 1px);
+  background-size: 60px 60px;
+}
+</style>
