91bee6a01f0a667dd944fd22fc2fd9e820b26000
siiiiisar <shunsuke.ue@gmail.com>
2026-04-30 12:56:36 +0900

    タグのstyle調整

--- src/components/BlogLayout.svelte
+++ src/components/BlogLayout.svelte
@@ -1,21 +1,9 @@
 <script lang="ts">
 	import dayjs from 'dayjs';
-	import { page } from '$app/stores';
 	import type { PageData } from '../routes/blogs/[slug]/$types';
 	import Tag from './Tag.svelte';
 
 	export let data: PageData;
-	let url = '';
-	let copied = false;
-	$: url = $page.url.href;
-
-	const copyToClipboard = () => {
-		navigator.clipboard.writeText(url);
-		copied = true;
-		setTimeout(() => {
-			copied = false;
-		}, 2000);
-	};
 </script>
 
 <article>
@@ -24,79 +12,8 @@
 			{data.title}
 		</h1>
 		<p class="mt-5 opacity-50">
-			{dayjs(data.publishedAt).format('YYYY/MM/DD')} 公開
+			{dayjs(data.publishedAt).format('YYYY/MM/DD')}
 		</p>
-		<div class="mt-5 flex justify-center space-x-4">
-			<script
-				type="text/javascript"
-				src="https://b.st-hatena.com/js/bookmark_button.js"
-				charset="utf-8"
-				async
-			></script>
-			<a
-				href="https://twitter.com/intent/tweet?url={url}&text={data.title}&via=siiiiisar"
-				role="button"
-				class="flex h-10 w-10 items-center justify-center rounded-full bg-gray-200 transition-colors hover:bg-gray-300"
-				target="_blank"
-				rel="noopener noreferrer"
-				aria-label="Twitterでシェア"
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="20"
-					height="20"
-					viewBox="0 0 512 512"
-					fill="currentColor"
-				>
-					<path
-						d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
-					/>
-				</svg>
-			</a>
-			<button
-				class="flex h-10 w-10 items-center justify-center rounded-full bg-gray-200 transition-colors hover:bg-gray-300"
-				on:click={copyToClipboard}
-				aria-label="URLをコピー"
-			>
-				{#if copied}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="20"
-						height="20"
-						fill="currentColor"
-						class="bi bi-clipboard-check"
-						viewBox="0 0 16 16"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0"
-						/>
-						<path
-							d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"
-						/>
-						<path
-							d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"
-						/>
-					</svg>
-				{:else}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="20"
-						height="20"
-						fill="currentColor"
-						class="bi bi-clipboard"
-						viewBox="0 0 16 16"
-					>
-						<path
-							d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"
-						/>
-						<path
-							d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"
-						/>
-					</svg>
-				{/if}
-			</button>
-		</div>
 	</header>
 	<div>
 		<div class="flex">
--- src/components/Tag.svelte
+++ src/components/Tag.svelte
@@ -2,9 +2,22 @@
 	export let href: string;
 </script>
 
-<a
-	{href}
-	class="rounded-full bg-gray-200 px-3 py-1 text-xs font-semibold text-gray-700 transition-colors duration-300 hover:bg-gray-300"
->
+<a {href} class="tag-link" target="_blank">
 	<slot />
 </a>
+
+<style>
+	.tag-link {
+		display: inline-flex;
+		align-items: center;
+		background: #8882;
+		line-height: 100%;
+		color: var(--fg-light) !important;
+		padding: 0.2rem;
+		border-radius: 0.25rem;
+	}
+	.tag-link:hover {
+		background: #8883;
+		color: var(--fg) !important;
+	}
+</style>
