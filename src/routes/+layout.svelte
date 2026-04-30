2d982f60d5480b76d595b39367dd404fd946e8c7
siiiiisar <shunsuke.ue@gmail.com>
2026-04-30 18:56:19 +0900

    背景を再追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,3 +15,18 @@
 	</div>
 </main>
 <Footer />
+
+<style>
+	.grid-bg {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: -1;
+		background-image:
+			linear-gradient(to right, #8882 1px, transparent 1px),
+			linear-gradient(to bottom, #8882 1px, transparent 1px);
+		background-size: 60px 60px;
+	}
+</style>
